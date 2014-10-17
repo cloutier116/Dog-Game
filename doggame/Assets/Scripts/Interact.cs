f047bcc00d7b254835d8bd5acb218ae40e0e6b6a
William Pheloung <wpheloung@gmail.com>
2014-10-17 18:22:04 -0400

    Initial commit, lacking proper gitignore!

---
+++ doggame/Assets/Scripts/Interact.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interact : MonoBehaviour {
+	private Transform myTransform;
+	public GameObject holding; //gameobject the player is currently holding
+	public float radius = 5.0f;
+	
+	// Use this for initialization
+	void Start () {
+		myTransform = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.G)) {
+			#if debugMode
+			Debug.Log ("G pressed, attempting to interact");
+			#endif
+			Collider[] hitColliders = Physics.OverlapSphere(myTransform.position, radius);
+			int i = 0;
+			while (i < hitColliders.Length) {
+				//hitColliders[i].SendMessage("AddDamage");
+				if(hitColliders[i].tag == "Interactive"){
+					#if debugMode
+					Debug.Log ("Interactive object is in range");
+					#endif
+				}
+				i++;
+			}
+		}
+	}
+}
