70a4b0597d2472d5be7077541eaac2ff81eef41f
William Pheloung <wpheloung@gmail.com>
2014-10-22 16:34:48 -0400

    Camera is good, controls are weird

--- doggame/Assets/CameraController.cs
+++ doggame/Assets/CameraController.cs
@@ -3,17 +3,32 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	public GameObject character;
+	public GameObject GameObject_target;
+	private Transform Transform_target;
+	private Vector3 Vector3_targetPosition;
+	private Quaternion Quaternion_targetRotation;
+
 	Transform tr;
+	
 
 	// Use this for initialization
 	void Start () {
 		tr = GetComponent<Transform> ();
+		GameObject_target = GameObject.FindGameObjectWithTag("CameraTarget");
+		Transform_target = GameObject_target.GetComponent<Transform>();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		tr.position = Vector3.Lerp (tr.position, new Vector3(character.transform.position.x, character.transform.position.y + 2, character.transform.position.z - 4) , Time.deltaTime*2);
-		tr.rotation = Quaternion.Slerp(tr.rotation, character.transform.rotation, Time.deltaTime);
+		
+	}
+
+	void FixedUpdate(){
+		Vector3_targetPosition = Transform_target.position ;
+		Quaternion_targetRotation = Transform_target.rotation;
+
+		tr.position = Vector3.Lerp (tr.position, Vector3_targetPosition, Time.deltaTime);
+		tr.rotation = Quaternion.Slerp(tr.rotation, Quaternion_targetRotation, Time.deltaTime*2);
 	}
 }
