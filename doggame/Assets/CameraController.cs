3637c73c99385d55ef7c669acce0b5afae7d927a
William Pheloung <wpheloung@gmail.com>
2014-10-22 16:23:47 -0400

    New Pursuit camera

--- doggame/Assets/CameraController.cs
+++ doggame/Assets/CameraController.cs
@@ -14,5 +14,6 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		tr.position = Vector3.Lerp (tr.position, new Vector3(character.transform.position.x, character.transform.position.y + 2, character.transform.position.z - 4) , Time.deltaTime*2);
+		tr.rotation = Quaternion.Slerp(tr.rotation, character.transform.rotation, Time.deltaTime);
 	}
 }
