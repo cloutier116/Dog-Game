6863849a4ac3ab318f5bfe8ca82df98663b07d6b
William Pheloung <wpheloung@gmail.com>
2014-10-22 14:09:48 -0400

    Tank style movement

--- doggame/Assets/PlatformerController.cs
+++ doggame/Assets/PlatformerController.cs
@@ -6,16 +6,21 @@ public class PlatformerController : MonoBehaviour {
 	public AnimationClip runAnimation;
 	public AnimationClip jumpAnimation;
 
-	public float runSpeed = 7.75f;
+	public float maxRunSpeed = 3.75f;
+	private float runSpeed = 0.0f;
+
 	public float jumpHeight = 0.5f;
 	public float jumpSpeed = 8.0f;
 	public float gravity = 20.0f;
+
+	private float vertical = 0.0f;
 
 	public bool climbing = false;
 	CharacterController controller;
 	Transform _transform;
 
 	private Vector3 moveDirection = Vector3.zero;
+	private Vector3 rotateDirection = Vector3.zero;
 
 	public CharacterState _characterState;
 
@@ -31,6 +36,23 @@ public class PlatformerController : MonoBehaviour {
 	}
 	
 	void Update() {
+
+		
+		vertical = Input.GetAxis ("Vertical");
+		Debug.Log ("vertical: "+ vertical *Time.deltaTime);
+		if(vertical > 0){
+			runSpeed += Time.deltaTime*1.5f;
+		}
+		else if(vertical <0){
+			runSpeed -= Time.deltaTime*1.5f;
+		}
+		else if(vertical == 0){
+			runSpeed *= 0.5f;
+		}
+		if(runSpeed > maxRunSpeed || runSpeed < -maxRunSpeed){
+			runSpeed = maxRunSpeed;
+		}
+
 		if (Input.GetKeyDown (KeyCode.C)) { //enable climbing
 			Collider[] hitColliders = Physics.OverlapSphere (_transform.position, 10.0f);
 			int i = 0;
@@ -56,7 +78,19 @@ public class PlatformerController : MonoBehaviour {
 		}
 		else{
 			if (controller.isGrounded) {
-				moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+				float horizontal = Input.GetAxis ("Horizontal");
+				Debug.Log ("runSpeed:" + runSpeed);
+				if(runSpeed < 0){
+					moveDirection = new Vector3 (0, 0, -runSpeed*Time.deltaTime*100.0f);
+				}
+				else{
+					moveDirection = new Vector3 (0, 0, runSpeed*Time.deltaTime*100.0f);
+				}
+				rotateDirection = new Vector3(0,horizontal*Time.deltaTime*100.0f,0);
+				
+				//rotate
+				transform.Rotate(rotateDirection);
+
 				moveDirection = transform.TransformDirection (moveDirection);
 				moveDirection *= runSpeed;
 				if (Input.GetButton ("Jump"))
