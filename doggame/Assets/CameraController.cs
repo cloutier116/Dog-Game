65c2df91cb6b45c9590f7084d9fb37ec9ec00e93
Chris <cloutc@rpi.edu>
2014-11-05 00:11:04 -0500

    walking toward the camera without clipping through the walls

--- doggame/Assets/CameraController.cs
+++ doggame/Assets/CameraController.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
+	public GameObject controller;
+	public Movement controller_movement;
 	public GameObject GameObject_target;
 	public Movement Movement_target;
 	public Transform Transform_target;
 	public Vector3 Vector3_targetPosition;
 	public Quaternion Quaternion_targetRotation;
+
 
 	public bool frozen = false;
 	
@@ -15,7 +18,9 @@ public class CameraController : MonoBehaviour {
 	public Transform Transform_climbingTarget;
 	public Vector3 Vector3_climbingTargetPosition;
 	public Quaternion Quaternion_climbingTargetRotation;
-	
+	public GameObject frontTarget;
+	public Transform frontTargetTr;
+	Transform dogTr;
 
 	Transform tr;
 	
@@ -25,7 +30,9 @@ public class CameraController : MonoBehaviour {
 		tr = GetComponent<Transform> ();
 		GameObject_target = GameObject.FindGameObjectWithTag("CameraTarget");
 		Transform_target = GameObject_target.GetComponent<Transform>();
+		dogTr = GameObject.FindGameObjectWithTag ("FrontCameraTarget").GetComponent<Transform>();
 		Movement_target = GameObject.FindGameObjectWithTag("MasterPlayer").GetComponent<Movement>();
+		controller_movement = controller.GetComponent<Movement> ();
 
 		
 		GameObject_climbingTarget = GameObject.FindGameObjectWithTag("ClimbingCameraTarget");
@@ -41,8 +48,17 @@ public class CameraController : MonoBehaviour {
 	void FixedUpdate(){
 
 		if(frozen){
-			Vector3_targetPosition = Transform_target.position;
-			Quaternion_targetRotation = Transform_target.rotation;
+			if(!controller_movement.down)
+			{
+				Vector3_targetPosition = Transform_target.position;
+				Quaternion_targetRotation = Transform_target.rotation;
+			}
+			else
+			{
+				Vector3 previousTarget = Vector3_targetPosition;
+				Vector3_targetPosition = previousTarget + controller_movement.walkDirection*1.05f;
+				Quaternion_targetRotation = tr.rotation;
+			}
 			float distance = Vector3.Distance(Vector3_targetPosition,tr.position);
 			distance += 0.5f;
 			
@@ -57,33 +73,71 @@ public class CameraController : MonoBehaviour {
 			if(futurePosition.magnitude > .02)
 			{
 				futurePosition = futurePosition.normalized * 5;
-				tr.position = Vector3.Lerp (tr.position, tr.position + futurePosition, Time.deltaTime*distance * 0.2f);
-				tr.rotation = Quaternion.Slerp(tr.rotation, Quaternion_targetRotation, Time.deltaTime * diffInRotation *2.0f);
+				if(controller_movement.down)
+				{
+					if(!Physics.Raycast(tr.position, futurePosition, .3f))
+					{
+						tr.position = Vector3.Lerp (tr.position, tr.position + futurePosition, Time.deltaTime*distance * 0.5f);
+						tr.rotation = Quaternion.Slerp(tr.rotation, Quaternion_targetRotation, Time.deltaTime * diffInRotation *2.0f);
+					}
+				}
+				else
+				{
+					tr.position = Vector3.Lerp (tr.position, tr.position + futurePosition, Time.deltaTime*distance * 0.5f);
+					tr.rotation = Quaternion.Slerp(tr.rotation, Quaternion_targetRotation, Time.deltaTime * diffInRotation *2.0f);
+				}
 			}
-
-		}
-		else{
-		if(Movement_target.climbing == false){
-			Vector3_targetPosition = Transform_target.position;
-			Quaternion_targetRotation = Transform_target.rotation;
-	
-			float distance = Vector3.Distance(Vector3_targetPosition,tr.position);
-			distance += 0.5f;
-
-			Quaternion currentRot = tr.rotation;
-			float diffInRotation = Mathf.Abs(currentRot.x - Quaternion_targetRotation.x) +
-				Mathf.Abs(currentRot.y - Quaternion_targetRotation.y) + 
-				Mathf.Abs(currentRot.z - Quaternion_targetRotation.z);
-			diffInRotation += 1.0f;
 			
-			Vector3 futurePosition = (Vector3_targetPosition - tr.position);
-			//print (futurePosition);
-			if(futurePosition.magnitude > .02)
+		}
+		else
+		{
+			if(Movement_target.climbing == false)
 			{
-				futurePosition = futurePosition.normalized * 5;
-				tr.position = Vector3.Lerp (tr.position, tr.position + futurePosition, Time.deltaTime*distance * 0.5f);
-				tr.rotation = Quaternion.Slerp(tr.rotation, Quaternion_targetRotation, Time.deltaTime * diffInRotation *2.0f);
-			}
+				if(!controller_movement.down)
+				{
+					Vector3_targetPosition = Transform_target.position;
+					Quaternion_targetRotation = Transform_target.rotation;
+				}
+				else
+				{
+					Vector3 previousTarget = Vector3_targetPosition;
+					Vector3_targetPosition = previousTarget + controller_movement.walkDirection*1.05f;
+					Quaternion_targetRotation = tr.rotation;
+				}
+				float distance = Vector3.Distance(Vector3_targetPosition,tr.position);
+				distance += 0.5f;
+
+				Quaternion currentRot = tr.rotation;
+				float diffInRotation = Mathf.Abs(currentRot.x - Quaternion_targetRotation.x) +
+					Mathf.Abs(currentRot.y - Quaternion_targetRotation.y) + 
+					Mathf.Abs(currentRot.z - Quaternion_targetRotation.z);
+				diffInRotation += 1.0f;
+				
+				Vector3 futurePosition = (Vector3_targetPosition - tr.position);
+				//print (futurePosition);
+				if(futurePosition.magnitude > .02)
+				{
+					futurePosition = futurePosition.normalized * 5;
+					if(controller_movement.down)
+					{
+						if(!Physics.Raycast(tr.position, futurePosition, .3f))
+						{
+							tr.position = Vector3.Lerp (tr.position, tr.position + futurePosition, Time.deltaTime*distance * 0.5f);
+							tr.rotation = Quaternion.Slerp(tr.rotation, Quaternion_targetRotation, Time.deltaTime * diffInRotation *2.0f);
+						}
+					}
+					else
+					{
+						tr.position = Vector3.Lerp (tr.position, tr.position + futurePosition, Time.deltaTime*distance * 0.5f);
+						tr.rotation = Quaternion.Slerp(tr.rotation, Quaternion_targetRotation, Time.deltaTime * diffInRotation *2.0f);
+					}
+				}
+				/*}
+				else
+				{
+					rigidbody.MovePosition(tr.position + controller_movement.walkDirection);
+					//tr.position = tr.position + controller_movement.walkDirection;
+				}*/
 		}
 		else{
 			
